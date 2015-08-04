a74b407ecb19d2cdf9477e7155936ad220e59dde
Nick Smith <Snickasaurus@users.noreply.github.com>
2015-08-04 00:00:40 -0400

    Create plugin-LastPass.sh

---
+++ plugin-LastPass.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Name    : plugin-LastPass.sh
+# Author  : Nick Smith
+# Date    : 20140808
+# Purpose : Detects if LastPass extension is installed for Firefox  
+
+# MyVariables
+currentUser=$(ls -l /dev/console | cut -d " " -f 4) 
+
+# Begin Script
+if [ -e /Users/$currentUser/Library/Application\ Support/Firefox/Profiles/*.default/extensions/support@lastpass.com ] #1
+	then
+theStatus="True"
+	else
+theStatus="False"
+
+fi
+
+echo "<result>$theStatus</result>"
+
+### Notes
+# 1 - You have to remove quotes if an asterick is used because quoting it will disable the 
+#     wildcard form of it and it will be treated litereally as part of the folder name.
