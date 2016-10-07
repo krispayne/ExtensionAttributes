14b80abb0ecb58c9f172acd1fd5493a4fdd414ae
Kris Payne <krispayne@me.com>
2016-10-07 16:25:34 -0700

    Update remove-SSIDFromPreferredList.sh

--- remove-SSIDFromPreferredList.sh
+++ remove-SSIDFromPreferredList.sh
@@ -4,32 +4,30 @@
 # Author  : Jacob Salmela
 # Date    : 20121126
 # Purpose : Removes a SSID from the list of preferred networks
+# Note    : Found @ https://jamfnation.jamfsoftware.com/discussion.html?id=5327
 
-	
-# Enter desired and undesired SSIDs here
+# Enter undesired SSIDs here
 undesiredNetwork="Undesired-SSID"
 
 # Determine if the interface is called Wi-Fi or Airport
-wifiOrAirport=$(/usr/sbin/networksetup -listallnetworkservices | grep -Ei '(Wi-Fi|AirPort)')
+wifiOrAirport=$(/usr/sbin/networksetup -listallnetworkservices | /usr/bin/grep -Ei '(Wi-Fi|AirPort)')
 
 # Determine what the device interface number is
-wirelessDevice=$(networksetup -listallhardwareports | awk "/$wifiOrAirport/,/Device/" | awk 'NR==2' | cut -d " " -f 2)
-	
-guestExists=$(/usr/sbin/networksetup -listpreferredwirelessnetworks "$wirelessDevice" | awk '"/$undesiredNetwork/" {print $1}')
+wirelessDevice=$(/usr/sbin/networksetup -listallhardwareports | /usr/bin/awk "/${wifiOrAirport}/,/Device/" | /usr/bin/awk 'NR==2' | /usr/bin/cut -d " " -f 2)
+
+# Does the undesired SSID exist?
+undesiredExists=$(/usr/sbin/networksetup -listpreferredwirelessnetworks "${wirelessDevice}" | /usr/bin/awk '"/${undesiredNetwork}/" {print $1}')
 
 function removeUndesiredNetwork() 
 	{
-	if [ -z "$guestExists" ];then
-		echo "Guest network does NOT exist."
+	if [ -z "${undesiredExists}" ];then
+		echo "Undesired network does NOT exist."
 		exit 0
 	else
-		echo "Guest Network found."
-		/usr/sbin/networksetup -removepreferredwirelessnetwork "$wirelessDevice" "$undesiredNetwork"
+		echo "Undesired Network found."
+		/usr/sbin/networksetup -removepreferredwirelessnetwork "${wirelessDevice}" "${undesiredNetwork}"
 		echo "Removed."
 	fi
 	}
 
 removeUndesiredNetwork
-
-# Notes:
-# Found @ https://jamfnation.jamfsoftware.com/discussion.html?id=5327
